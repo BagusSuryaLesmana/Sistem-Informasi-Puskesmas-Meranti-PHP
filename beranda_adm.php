e9a976c310ab900106b4709d5c36749ae43b57b5
BagusSuryaLesmana <46617825+BagusSuryaLesmana@users.noreply.github.com>
2019-01-26 00:52:33 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Arsip Puskesmas Meranti</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/lambangbarupkm.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>PUSKESMAS MERANTI</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Arsip Puskesmas Meranti.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data arsip.<br><br>
+                            
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data arsip.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
---
+++ views/about_tampil.php
@@ -0,0 +1,33 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <p align="center"><img src="img/lambangbarupkm.jpg"></img></p>
+                            <label class="col-sm-12 control-label"><center><strong>SISTEM INFORMASI ARSIP <br> PUSKESMAS MERANTI</strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Sistem Informasi Arsip Puskesmas Meranti merupakan sebuah sistem informasi yang dirancang untuk membantu
+                            karyawan lebih cepat mencari data.
+                            <br><br><br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
